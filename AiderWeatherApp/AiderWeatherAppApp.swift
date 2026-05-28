79bf1af758312b0641783c04b63647cd1d08d70a
Masaya Nakakuki <masa8y88@gmail.com>
2026-05-28 13:49:55 +0900

    Initial Commit

---
+++ AiderWeatherApp/AiderWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiderWeatherAppApp.swift
+//  AiderWeatherApp
+//
+//  Created by Masaya Nakakuki on 2026/05/28.
+//
+
+import SwiftUI
+
+@main
+struct AiderWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AiderWeatherApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AiderWeatherApp
+//
+//  Created by Masaya Nakakuki on 2026/05/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
